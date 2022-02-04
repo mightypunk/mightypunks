832e74c3db6fbd1a36a0fbdc437cb525ad727eeb
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-04 15:47:49 +0700

    create network image asset

---
+++ lib/constants/network_image_assets.dart
@@ -0,0 +1,26 @@
+abstract class NetworkImageAssets {
+  static const List<String> gifs = [
+    "GIF-220109_082316.gif",
+    "GIF-220126_191419.gif",
+    "GIF-220113_095442.gif",
+    "GIF-220127_141746.gif",
+    "GIF-220113_155543.gif",
+    "GIF-220128_174441.gif",
+    "GIF-220117_114833.gif",
+    "GIF-220129_171611.gif",
+    "GIF-220118_120632.gif",
+    "GIF-220130_181637.gif",
+    "GIF-220123_141153.gif",
+    "GIF-220130_230018.gif",
+    "GIF-220124_121233.gif",
+    "GIF-220131_194746.gif",
+    "GIF-220124_170912.gif",
+    "GIF-220125_161155.gif",
+    "GIF-220125_183934.gif",
+    "GIF-220125_222442.gif",
+  ];
+  static const banner = "MightyPunkBanner.png";
+  static const outline = "MightyPunksShadowOutline.png";
+  static const outlineSheer = "MightyPunksShadowOutlineSheer7.png";
+  static const logo = "Tak%20berjudul22_20220113140343.png";
+}
